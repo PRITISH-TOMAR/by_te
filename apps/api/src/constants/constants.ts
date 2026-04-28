848481db656bfb02ed96f05faf4f2937c6d0faeb
PRITISH TOMAR <pritish.t@osmosys.co>
2026-04-28 22:04:39 +0530

    add-crypto-package

---
+++ apps/api/src/constants/constants.ts
@@ -0,0 +1,4 @@
+export const constants = {
+    CHARSET :"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_",
+    CODE_LENGTH : 6,
+}
--- apps/api/src/constants/error.ts
+++ apps/api/src/constants/error.ts
@@ -1,3 +1,6 @@
 export const ERROR = {
   HEALTH_CHECK_FAILURE: "HEALTH_CHECK_FAILURE",
+
+
+  CUSTOM_ALIAS_ALREADY_EXIST: "Custom alias already taken",
 } as const;
