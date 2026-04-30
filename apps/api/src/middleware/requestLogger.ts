81db1250c66b75418da56deee9132f42e8519bce
PRITISH TOMAR <pritish.t@osmosys.co>
2026-04-30 22:01:06 +0530

    update-request-logger

--- apps/api/src/middleware/requestLogger.ts
+++ apps/api/src/middleware/requestLogger.ts
@@ -1,26 +1,31 @@
 import { Request, Response, NextFunction } from "express";
+import "../types/express";
 import { v4 as uuidv4 } from "uuid";
 import logger from "../config/logger";
 
 // Attach tracebackId to every request
-export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const tracebackId = uuidv4();
   const start = Date.now();
 
   // Attach to request so it can be used in services
-  (req as any).tracebackId = tracebackId;
+  req.tracebackId = tracebackId;
 
   res.on("finish", () => {
     logger.info({
-      message:     "Incoming request",
-      source:      "requestLogger middleware",
+      message: "Incoming request",
+      source: "requestLogger middleware",
       tracebackId,
-      url:         req.originalUrl,
-      httpMethod:  req.method,
+      url: req.originalUrl,
+      httpMethod: req.method,
       data: {
-        status:    res.statusCode,
-        duration:  `${Date.now() - start}ms`,
-        ip:        req.ip,
+        status: res.statusCode,
+        duration: `${Date.now() - start}ms`,
+        ip: req.ip,
       },
     });
   });
