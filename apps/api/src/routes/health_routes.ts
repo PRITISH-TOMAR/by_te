af227882a221a9f37538feebd6f3f708a9ff5576
PRITISH TOMAR <pritish.t@osmosys.co>
2026-05-01 00:31:51 +0530

    updqate-debugger-support

--- apps/api/src/routes/health_routes.ts
+++ apps/api/src/routes/health_routes.ts
@@ -2,6 +2,6 @@ import express, { Router, Request, Response } from "express";
 import { checkHealth } from "../services/health_service";
 const router: Router = express.Router();
 
-router.get("/health", (req: Request, res: Response) => checkHealth(res));
+router.get("/", (req: Request, res: Response) => checkHealth(res));
 
 export default router;
