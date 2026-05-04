a3abbabafc4fdbdcbbc3c6c9f1e3e27739ad22d3
PRITISH TOMAR <pritish.t@osmosys.co>
2026-05-04 22:18:58 +0530

    add-auth-on-resource-post-route

--- apps/api/src/routes/resource_routes.ts
+++ apps/api/src/routes/resource_routes.ts
@@ -1,11 +1,17 @@
 import express, { Router, Request, Response } from "express";
 import { ShortenRequestSchema } from "../dto/request/resources/shorten_request_dto";
+import { authenticateJwt } from "../middleware/authenticateJwt";
 import { validateBody } from "../middleware/validateBody";
 import { getResource, ShortenResource } from "../services/resource_service";
 
 const router: Router = express.Router();
 
-router.post("/", validateBody(ShortenRequestSchema), ShortenResource);
+router.post(
+  "/",
+  authenticateJwt,
+  validateBody(ShortenRequestSchema),
+  ShortenResource,
+);
 router.get("/:shortCode", getResource);
 
 export default router;
